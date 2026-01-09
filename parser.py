901c97e82c7be78015391748662248ad5b82e92c
yuliiakadyhrob <yuliiakadyhrob@gmail.com>
2026-01-09 23:44:28 +0200

    Update parser.py

---
+++ parser.py
@@ -0,0 +1,8 @@
+def search_court_case(person_name: str):
+    # Временная заглушка для MVP
+    return [
+        {
+            "title": f"Судова справа щодо {person_name}",
+            "link": "https://reyestr.court.gov.ua/"
+        }
+    ]
